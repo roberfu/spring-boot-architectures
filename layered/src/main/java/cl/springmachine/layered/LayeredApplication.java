ff5ef384ad67ebd22262c12611883be4afc8698d
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2024-01-30 03:10:05 -0300

    Init project

---
+++ layered/src/main/java/cl/springmachine/layered/LayeredApplication.java
@@ -0,0 +1,13 @@
+package cl.springmachine.layered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayeredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayeredApplication.class, args);
+	}
+
+}
---
+++ onion/src/main/java/cl/springmachine/onion/OnionApplication.java
@@ -0,0 +1,13 @@
+package cl.springmachine.onion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnionApplication.class, args);
+	}
+
+}
