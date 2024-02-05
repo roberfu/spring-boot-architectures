bde0c849610f5f2e661a168f6f87a85c0cd8afd5
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2024-02-05 12:08:12 -0300

    Rename package declaration

--- onion/src/main/java/cl/springmachine/onion/domain/pokemon/Pokemon.java
+++ onion/src/main/java/cl/springmachine/onion/domain/pokemon/Pokemon.java
@@ -1,17 +1,19 @@
 package cl.springmachine.onion.domain.pokemon;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Pokemon {
 
-    private Integer id;
+	private Integer id;
 
-    private String name;
+	private String name;
 
-    private Integer pokedexNumber;
+	private Integer pokedexNumber;
 }
