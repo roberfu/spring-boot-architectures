b31ad98758c45cf0690644ec175d3680bb564022
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2024-02-05 19:22:28 -0300

    Init modulith project

---
+++ modulith/src/test/java/cl/springmachine/modulith/ModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.springmachine.modulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
